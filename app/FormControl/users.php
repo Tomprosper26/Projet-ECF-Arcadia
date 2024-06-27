c1b20830be9ed6bf8ab08782b16f43e09e90d8da
Tomprosper26 <99676630+Tomprosper26@users.noreply.github.com>
2024-06-27 12:03:06 +0200

    Update users.php

--- app/FormControl/users.php
+++ app/FormControl/users.php
@@ -13,49 +13,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($action == 'create_user') {
 
-        $message = "Votre identifiant de connection au site est : " . $_POST['email'] . "pour votre mot De passe merci de vous rapprocher de José";
-        $subject = "Mon Sujet";
+        $message = "Votre identifiant de connection au site est : " . $_POST['email'] . " pour votre mot De passe merci de vous rapprocher de José";
+        $subject = "Arcadia Compte Employe";
 
         $mail = new PHPMailer(true);
+	  
+		$username = $_POST['email'];
+		$testemail = $userDAO->getUserByEmail($username);
+		$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+		$role_id = $_POST['role_id'];
+		$nom = $_POST['nom'];
+		$prenom = $_POST['prenom'];
+		  
+		if(!empty($testemail)){
+	
+			echo
+			"<div class=\" container \">
+				<div class=\"row justify-content-center mt-5\">   
+					<div class=\"alert alert-danger text-center col-xl-6 col-md-8 col-sm-10\" role=\"alert\">
+						cette adresse mail est déja utilisé.
+					</div>
+				</div>
+			</div>";
+	
+		} else {
+	
+			$userDAO->createUser($username, $password, $nom, $prenom, $role_id);
+				
+			try {
+				// Paramètres du serveur
+				$mail->isSMTP();
+				$mail->Host = 'mail.infomaniak.com';
+				$mail->SMTPAuth = true;
+				$mail->Username = "arcadiazoo@mayto.fr";
+				$mail->Password = "Dcaf3d446a";
+				$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+				$mail->Port = 465;
+	
+				// Destinataires
+				$mail->setFrom('arcadiazoo@mayto.fr', 'ArcadiaZoo');
+				$mail->addAddress($_POST['email'], 'Destinataire');
+	
+				// Contenu de l'email
+				$mail->isHTML(true);
+				$mail->Subject = $subject;
+				$mail->Body    = $message;
+				$mail->AltBody = $message;
+	
+				$mail->send();
+				echo 'Email envoyé avec succès.';
+			} catch (Exception $e) {
+				echo "Échec de l'envoi de l'email. Erreur: {$mail->ErrorInfo}";
+			}
+	
+	
+			$url = "/connexion-" . $user['role_id'];
+			header("Location: $url");
+			exit();
+	
+		}
 
-        try {
-            // Paramètres du serveur
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = "";
-            $mail->Password = "";
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $mail->Port = 587;
-
-            // Destinataires
-            $mail->setFrom('acradiazoo@gmail.com', 'Expéditeur');
-            $mail->addAddress($_POST['email'], 'Destinataire');
-
-            // Contenu de l'email
-            $mail->isHTML(true);
-            $mail->Subject = $subject;
-            $mail->Body    = $message;
-            $mail->AltBody = $message;
-
-            $mail->send();
-            echo 'Email envoyé avec succès.';
-        } catch (Exception $e) {
-            echo "Échec de l'envoi de l'email. Erreur: {$mail->ErrorInfo}";
-        }
-
-
-        $username = $_POST['email'];
-        $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-        $role_id = $_POST['role_id'];
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prenom'];
-
-        $userDAO->createUser($username, $password, $nom, $prenom, $role_id);
-
-        $url = "/connexion-" . $user['role_id'];
-        header("Location: $url");
-        exit();
 
     }
 }
